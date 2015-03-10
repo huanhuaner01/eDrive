61b38bb79813bcee81c00b2408b1dfc79e0b66d1
huanhuaner01 <916243763@qq.com>
2015-03-10 19:19:40 +0800

    不滚动的列表。

---
+++ eDrive/src/com/huishen/edrive/widget/NoScrollListView.java
@@ -0,0 +1,32 @@
+package com.huishen.edrive.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/***
+ * 自定义ListView子类，继承ListView
+ * @author Administrator
+ *
+ */
+public class NoScrollListView extends ListView {
+
+	public NoScrollListView(Context context) {
+		super(context);
+	}
+
+	public NoScrollListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public NoScrollListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
+	}
+
+}
