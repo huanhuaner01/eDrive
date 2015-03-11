28371be2e9580fb3a30862cf33372f4a01b55ab7
huanhuaner01 <916243763@qq.com>
2015-03-11 12:31:15 +0800

    为啥每次的新文件都需要我自己去添加呢。

---
+++ eDrive/src/com/huishen/edrive/InfoActivity.java
@@ -0,0 +1,56 @@
+package com.huishen.edrive;
+
+import com.huishen.edrive.net.NetUtil;
+import com.huishen.edrive.util.AppController;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class InfoActivity extends Activity {
+	private WebView webview  ;
+    private TextView  title ;
+    private ImageButton back ;
+    private String titlestr ,url ;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_info);
+		AppController.getInstance().addActivity(this);
+		//-----------------获取数据 --------------------
+		titlestr = this.getIntent().getStringExtra("title") ;
+		url = this.getIntent().getStringExtra("url");
+		//-------------------获取数据结束！---------------
+		registView();
+		init();
+	}
+	private void registView() {
+		title = (TextView) findViewById(R.id.header_title);
+		back = (ImageButton) findViewById(R.id.header_back);
+		webview = (WebView) findViewById(R.id.service_info_webview);
+	}
+	private void init() {
+		this.title.setText(titlestr) ;
+		this.back.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				finish();
+			}
+		});
+		webview.getSettings().setJavaScriptEnabled(true);
+		openUrl(NetUtil.getAbsolutePath(url)) ;
+	}
+	
+	/**
+	 * 打开一个网站
+	 * @param url
+	 */
+	private void openUrl(String url){
+//		Log.i("ServiceInfoActivity",url) ;
+		webview.loadUrl(url);
+	}
+}
